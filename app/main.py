a91ec4f660a0a384842d644eed4b2219afcd54f7
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2026-04-29 18:43:39 -0600

    Creación y subscripción de planes y clientes en FastAPI

--- app/main.py
+++ app/main.py
@@ -7,12 +7,13 @@ from db import create_all_tables
 from fastapi import FastAPI
 
 from models import  Transaction, Invoice
-from .routers import customers, transactions
+from .routers import customers, transactions, plans
     
 
 app = FastAPI(lifespan=create_all_tables)
 app.include_router(customers.router)
 app.include_router(transactions.router)
+app.include_router(plans.router)
 
 @app.get("/")
 async def root():
--- app/routers/customers.py
+++ app/routers/customers.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, status, HTTPException
 from sqlmodel import select
 
-from models import Customer,CustomerCreate, CustomerUpdate
+from models import Customer,CustomerCreate, CustomerPlan, CustomerUpdate, Plan
 from db import SessionDep
 
 router = APIRouter(tags=['customers'])
@@ -48,3 +48,27 @@ async def delete_customer(id: int, session: SessionDep):
     session.commit()
     return {'detail':'ok'}
     
+@router.post('/customers/{customer_id}/plans/{plan_id}')
+async def subscribe_customer_to_plan(customer_id: int,plan_id: int, session: SessionDep):
+    customer_db = session.get(Customer,customer_id)
+    plan_db = session.get(Plan,plan_id)
+    
+    if not customer_db or not plan_db:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='The customer or plan doesnt exist'
+        )
+    
+    customer_plan_db =CustomerPlan(plan_id=plan_db.id,customer_id=customer_db.id)
+    
+    session.add(customer_plan_db)
+    session.commit()
+    session.refresh(customer_plan_db)
+    return customer_plan_db
+
+@router.get('/customers/{customer_id}/plans/')
+async def get_subscriptions(customer_id: int ,session: SessionDep):
+    customer = session.get(Customer,customer_id)
+    if customer == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='Customer not found')
+    return customer.plans
